1f7eafce66ead8cf2747b578e1d189839dbfdaeb
harrisishaq <mynameharris7@gmail.com>
2024-03-10 00:06:37 +0800

    Add - Controller Function for Category

---
+++ controller/category_controller.go
@@ -0,0 +1,113 @@
+package controller
+
+import (
+	"net/http"
+	"test_project/model"
+	"test_project/service"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type categoryController struct {
+	service service.CategoryService
+}
+
+func NewCategoryController(service service.CategoryService) *categoryController {
+	return &categoryController{service}
+}
+
+func (controller *categoryController) CategoryRoutes(e *echo.Echo) {
+	e.Use(middleware.CORS())
+
+	// Category EP
+	var categoryRoute = e.Group("/categories")
+	categoryRoute.POST("/", controller.CreateCategory)
+	categoryRoute.POST("/list", controller.ListCategory)
+	categoryRoute.DELETE("/:id", controller.DeleteCategory)
+	categoryRoute.GET("/:id", controller.GetCategory)
+	categoryRoute.PUT("/:id", controller.UpdateCategory)
+}
+
+func (ctrl *categoryController) CreateCategory(c echo.Context) error {
+	request := new(model.CreateCategoryRequest)
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	} else if err := c.Validate(request); err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", err.Error()))
+	}
+
+	err := ctrl.service.CreateCategory(request)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.(*model.JsonResponse))
+	}
+
+	return c.JSON(http.StatusOK, model.NewJsonResponse(true))
+}
+
+func (ctrl *categoryController) DeleteCategory(c echo.Context) error {
+	id := c.Param("id")
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	}
+
+	err := ctrl.service.DeleteCategory(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.(*model.JsonResponse))
+	}
+
+	return c.JSON(http.StatusOK, model.NewJsonResponse(true))
+}
+
+func (ctrl *categoryController) GetCategory(c echo.Context) error {
+	id := c.Param("id")
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	}
+
+	data, err := ctrl.service.GetCategory(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.(*model.JsonResponse))
+	}
+
+	return c.JSON(http.StatusOK, model.NewJsonResponse(true).SetData(data))
+}
+
+func (ctrl *categoryController) ListCategory(c echo.Context) error {
+	request := new(model.ListCategoryRequest)
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	}
+
+	data, total, err := ctrl.service.ListCategory(*request)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.(*model.JsonResponse))
+	}
+
+	return c.JSON(http.StatusOK, model.NewJsonResponse(true).List(data, total))
+}
+
+func (ctrl *categoryController) UpdateCategory(c echo.Context) error {
+	id := c.Param("id")
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	}
+
+	request := new(model.UpdateCategoryRequest)
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", "Bad Request"))
+	} else if err := c.Validate(request); err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewJsonResponse(false).SetError("400", err.Error()))
+	}
+
+	request.ID = id
+	err := ctrl.service.UpdateCategory(request)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.(*model.JsonResponse))
+	}
+
+	return c.JSON(http.StatusOK, model.NewJsonResponse(true))
+}
